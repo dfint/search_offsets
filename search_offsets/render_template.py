34abec47ae8d7ac30220935cd8aec286003c5beb
insolor <2442833+insolor@users.noreply.github.com>
2024-09-05 15:59:46 +0300

    Updates for beta22 and 50.14testing

--- search_offsets/render_template.py
+++ search_offsets/render_template.py
@@ -7,14 +7,25 @@ def hex_filter(value: int) -> str:
     """
     jinja2 filter to convert an integer to a hexadecimal string.
     """
-    return f"0x{value:X}"
+    try:
+        return f"0x{value:X}"
+    except TypeError:
+        return value
+
+
+def init_jinja_env() -> Environment:
+    """
+    Initialize jinja2 environment.
+    """
+    env = Environment(loader=PackageLoader("search_offsets"), autoescape=select_autoescape())
+    env.filters["hex"] = hex_filter
+    return env
 
 
 def render_template(found: Mapping[str, list[int]], checksum: int, version_name: str) -> str:
     """
     Render the template with the found offsets.
     """
-    env = Environment(loader=PackageLoader("search_offsets"), autoescape=select_autoescape())
-    env.filters["hex"] = hex_filter
+    env = init_jinja_env()
     template = env.get_template("offsets.toml.jinja")
     return template.render(**found, checksum=checksum, version_name=version_name)
