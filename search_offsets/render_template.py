a3424e95b033d021bf38509eddfaf4a8a2078672
insolor <2442833+insolor@users.noreply.github.com>
2025-02-13 18:35:12 +0300

    Implement generation of offsets file for the linux version of DF

--- search_offsets/render_template.py
+++ search_offsets/render_template.py
@@ -1,5 +1,3 @@
-from collections.abc import Mapping
-
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 
@@ -20,12 +18,3 @@ def init_jinja_env() -> Environment:
     env = Environment(loader=PackageLoader("search_offsets"), autoescape=select_autoescape())
     env.filters["hex"] = hex_filter
     return env
-
-
-def render_template(found: Mapping[str, list[int]], checksum: int, version_name: str) -> str:
-    """
-    Render the template with the found offsets.
-    """
-    env = init_jinja_env()
-    template = env.get_template("windows_offsets.toml.jinja")
-    return template.render(**found, checksum=checksum, version_name=version_name)
