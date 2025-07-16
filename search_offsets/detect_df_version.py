46c383acea11b40ce7ec88037ef52b954a878016
insolor <2442833+insolor@users.noreply.github.com>
2025-07-16 18:04:32 +0300

    Add test for ordering, don't include beta text in ordering

--- search_offsets/detect_df_version.py
+++ search_offsets/detect_df_version.py
@@ -5,13 +5,14 @@ pattern = re.compile(rb"\x00((\d+)\.(\d+)(\-([^\d]+)(\d*))?)\x00")
 MAX_BETA = 10000
 
 
-def version_comparing_key(value: re.Match[bytes]) -> tuple[int, int, str, int]:
+def version_comparing_key(value: re.Match[bytes]) -> tuple[int, int, int]:
     """
     Return a tuple of (major, minor, beta) used for detecting max version number in the executable.
     """
-    beta_text = value.group(5)
+    version_major = int(value.group(2))
+    version_minor = int(value.group(3))
     beta_number = value.group(6) or MAX_BETA
-    return int(value.group(2)), int(value.group(3)), beta_text, int(beta_number)
+    return version_major, version_minor, int(beta_number)
 
 
 def detect_df_version(data: bytes) -> str:
--- tests/test_detect_df_version.py
+++ tests/test_detect_df_version.py
@@ -1,6 +1,8 @@
+import re
+
 import pytest
 
-from search_offsets.detect_df_version import detect_df_version
+from search_offsets.detect_df_version import detect_df_version, pattern, version_comparing_key
 
 
 @pytest.mark.parametrize(
@@ -15,3 +17,17 @@ from search_offsets.detect_df_version import detect_df_version
 )
 def test_detect_df_version(data: bytes, expected: str | None) -> None:
     assert detect_df_version(data) == expected
+
+
+@pytest.mark.parametrize(
+    ("left", "right", "greater"),
+    [
+        (b"\x0051.01\x00", b"\x0051.01-beta1\x00", True),
+    ],
+)
+def test_version_comparing_key(*, left: bytes, right: bytes, greater: bool):
+    match_left = pattern.search(left)
+    assert match_left is not None
+    match_right = pattern.search(right)
+    assert match_right is not None
+    assert (version_comparing_key(match_left) > version_comparing_key(match_right)) == greater
