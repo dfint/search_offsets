2e0deec83433abde9351faba144ce9c05b9ea699
insolor <2442833+insolor@users.noreply.github.com>
2025-07-16 16:08:01 +0300

    Update pattern to support versions like 51.13-lua+filesystem-test-1

--- search_offsets/detect_df_version.py
+++ search_offsets/detect_df_version.py
@@ -1,6 +1,6 @@
 import re
 
-pattern = re.compile(rb"\x00((\d+)\.(\d+)(\-beta(\d+))?)\x00")
+pattern = re.compile(rb"\x00((\d+)\.(\d+)(\-([^\d]+)(\d*))?)\x00")
 
 MAX_BETA = 10000
 
@@ -9,8 +9,9 @@ def version_comparing_key(value: re.Match[bytes]) -> tuple[int, int, int]:
     """
     Return a tuple of (major, minor, beta) used for detecting max version number in the executable.
     """
-    beta = value.group(5) or MAX_BETA
-    return int(value.group(2)), int(value.group(3)), int(beta)
+    beta_text = value.group(5)
+    beta_number = value.group(6) or MAX_BETA
+    return int(value.group(2)), int(value.group(3)), beta_text, int(beta_number)
 
 
 def detect_df_version(data: bytes) -> str:
--- tests/test_detect_df_version.py
+++ tests/test_detect_df_version.py
@@ -8,8 +8,9 @@ from search_offsets.detect_df_version import detect_df_version
     [
         (b"\x0051.01-beta1\x00", "51.01-beta1"),
         (b"\x0051.02\x00", "51.02"),
-        (b"\x0051.03-foo\x00", None),
+        (b"\x0051.03-foo\x00", "51.03-foo"),
         (b"\x0051.05\x00\x0051.01\x00\x0050.01\x00", "51.05"),
+        (b"\x0051.13-lua+filesystem-test-1\x00", "51.13-lua+filesystem-test-1"),
     ],
 )
 def test_detect_df_version(data: bytes, expected: str | None) -> None:
